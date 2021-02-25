2645cd1633a697134926a931349139cfa1fd61c4
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-02-25 21:17:13 +0530

    resolve staticfiles conflict

--- EcommerceApp/settings.py
+++ EcommerceApp/settings.py
@@ -126,9 +126,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/"
 MEDIA_ROOT = BASE_DIR
