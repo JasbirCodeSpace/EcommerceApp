e2846e6351b95b1792685b0c489f7eadc77f50e8
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2021-02-25 21:03:34 +0530

    Added configuration for heroku

--- EcommerceApp/settings.py
+++ EcommerceApp/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p47$qde%l@n7x#y7s@oy+xr-ocf##_76ll&t1$et&om2h2&=w8'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ['DEBUG_VALUE'] == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djang-store-app.herokuapp.com']
 
 
 # Application definition
@@ -124,8 +125,13 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/"
 MEDIA_ROOT = BASE_DIR
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+django_heroku.settings(locals())
